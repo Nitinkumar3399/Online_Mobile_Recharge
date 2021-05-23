71a5c0b84ee5b7c34cb0e7dbb777cb77f859f301
Nitin Kumar <65066519+Nitinkumar3399@users.noreply.github.com>
2021-05-24 01:02:59 +0530

    Add files via upload

---
+++ PaytmKit/include/nav.php
@@ -0,0 +1,8 @@
+ <center> 
+        <nav class="navbar">
+          <a class="navbar-brand " href="kart.php">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Paytm_logo.jpg" width="100" height="30" class="d-inline-block align-top" alt="">
+             Payment Integration 
+          </a>
+        </nav>
+    </center>
